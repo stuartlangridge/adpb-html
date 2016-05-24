f0a07ac40b799883e2c7e94d05d7ed18ae734342
Stuart Langridge <sil@kryogenix.org>
2016-05-24 09:45:31 +0100

    Import

--- output/theme/live-webmentions.js
+++ output/theme/live-webmentions.js
@@ -21,79 +21,107 @@ function wm_rx_live_update_prompted(data) {
     wm_rx_live_update(data);
 }
 
-function wm_rx_live_update(data) {
-    if (data && data.query && data.query.results && data.query.results.json) {
-        var j = data.query.results.json;
-        if (j.json) j = j.json;
-        if (!Array.isArray(j)) {
-            if (j.url) {
-                j = [j];
-            } else {
-                return;
-            }
+function wm_rx_live_update(j) {
+    if (!Array.isArray(j)) { return; }
+    var ul = document.querySelector("#webmentions ul");
+    ul.innerHTML = "";
+    var likes_reposts = [];
+    j.forEach(function(wm) {
+        var imgsrc;
+        if (wm.author && wm.author.photo && wm.author.photo != "null") {
+            imgsrc = wm.author.photo;
+        } else {
+            imgsrc = "http://www.gravatar.com/avatar/no?d=mm";
         }
-        var ul = document.querySelector("#webmentions ul");
-        ul.innerHTML = "";
-        j.forEach(function(wm) {
-            /* bouncing the webmentions heroku response off YQL to avoid cross-origin problems
-               seems to end up with null values being "null" instead. Tch, eh? */
-            var li = document.createElement("li");
-            
-            var img = document.createElement("img"); 
-            if (wm.author && wm.author.photo && wm.author.photo != "null") {
-                img.src = wm.author.photo;
-            } else {
-                img.src = "http://www.gravatar.com/avatar/no?d=mm";
-            }
-            li.appendChild(img);
+        var wmaname;
+        if (wm.author && wm.author.name && wm.author.name != "null") {
+            wmaname = wm.author.name;
+        }
 
-            var tn;
-            if (wm.author && wm.author.name && wm.author.name != "null") {
-                tn = " " + wm.author.name + " responded at ";
-            } else {
-                tn = " A response was written at ";
-            }
-            li.appendChild(document.createTextNode(tn));
+        if (wm.type == "like" || wm.type == "repost") {
+            likes_reposts.push({imgsrc: imgsrc, name: wmaname, url: wm.url});
+            return;
+        }
 
+        var li = document.createElement("li");
+        
+        var img = document.createElement("img"); 
+        img.src = imgsrc;
+        li.appendChild(img);
+
+        var tn;
+        if (wmaname) {
+            tn = " " + wmaname + " responded at ";
+        } else {
+            tn = " A response was written at ";
+        }
+        li.appendChild(document.createTextNode(tn));
+
+        var a = document.createElement("a");
+        a.href = wm.url;
+        var netloc = wm.url.match(/:\/\/([^\/]+)/)[1];
+        if (netloc.length > 20) {
+            netloc = netloc.substr(0,20) + "...";
+        }
+        var wm_name = wm.name || "null";
+        if (wm_name.length > 20) {
+            wm_name = wm_name.substr(0,20) + "...";
+        }
+        if (wm.name && wm.name != "null") {
+            a.appendChild(document.createTextNode(wm_name));
+            li.appendChild(a);
+            li.appendChild(document.createTextNode(" (" + netloc + ") "));
+        } else {
+            a.appendChild(document.createTextNode(netloc));
+            li.appendChild(a);
+        }
+
+        if (wm.summary && wm.summary !== "null") {
+            var sp = document.createElement("span");
+            sp.className = "wm-summary";
+            if (wm.summary.length > 150) {
+                sp.appendChild(document.createTextNode(wm.summary.substr(0,150) + "..."));
+            } else {
+                sp.appendChild(document.createTextNode(wm.summary));
+            }
+            li.appendChild(sp);
+        }
+        ul.appendChild(li);
+    });
+    if (likes_reposts.length > 0) {
+        var li = document.createElement("li");
+        li.appendChild(document.createTextNode("Liked/reposted by: "));
+        likes_reposts.forEach(function(lr) {
             var a = document.createElement("a");
-            a.href = wm.url;
-            var netloc = wm.url.match(/:\/\/([^\/]+)/)[1];
-            if (netloc.length > 20) {
-                netloc = netloc.substr(0,20) + "...";
-            }
-            var wm_name = wm.name;
-            if (wm_name.length > 20) {
-                wm_name = wm_name.substr(0,20) + "...";
-            }
-            if (wm.name && wm.name != "null") {
-                a.appendChild(document.createTextNode(wm_name));
-                li.appendChild(a);
-                li.appendChild(document.createTextNode(" (" + netloc + ") "));
-            } else {
-                a.appendChild(document.createTextNode(netloc));
-                li.appendChild(a);
-            }
-
-            if (wm.summary && wm.summary !== "null") {
-                var sp = document.createElement("span");
-                sp.className = "wm-summary";
-                if (wm.summary.length > 150) {
-                    sp.appendChild(document.createTextNode(wm.summary.substr(0,150) + "..."));
-                } else {
-                    sp.appendChild(document.createTextNode(wm.summary));
-                }
-                li.appendChild(sp);
-            }
-            ul.appendChild(li);
+            a.href = lr.url;
+            var img = document.createElement("img");
+            img.src = lr.imgsrc;
+            a.appendChild(img);
+            a.setAttribute("title", lr.name);
+            li.appendChild(a);
+            li.appendChild(document.createTextNode(" "));
         });
+        ul.appendChild(li);
     }
 }
-function live_update_wm(cbname) {
-    var s = document.createElement("script");
-    s.src = "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20json%20where%20" +
-        "url%3D%22https%3A%2F%2Fwebmention.herokuapp.com%2Fapi%2Fmentions%3Furl%3D" +
-        encodeURIComponent(location.href) +
-        "%22%20&format=json&callback=" + cbname + "&rnd=" + Math.random();
-    document.getElementsByTagName("head")[0].appendChild(s);
+function live_update_wm(cb) {
+    var x = new XMLHttpRequest();
+    var pageURL = location.href;
+    // localhost debugging
+    pageURL = pageURL.replace(/localhost:[0-9]+\//, "www.kryogenix.org/days/");
+    pageURL = pageURL.replace(/file:\/\/.*githubrepo\/output\//, "https://www.kryogenix.org/days/");
+    x.open("GET", "https://crossorigin.me/https://webmention.herokuapp.com/api/mentions?url=" + pageURL, true);
+    x.onreadystatechange = function() {
+        if (x.readyState == 4) {
+            var j;
+            try {
+                j = JSON.parse(x.responseText);
+            } catch(e) {
+                return;
+            }
+            cb(j);
+        }
+    };
+    x.send();
 }
-live_update_wm("wm_rx_live_update");
+live_update_wm(wm_rx_live_update);
